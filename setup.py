4c83e310058f55294923ca2f717683749bb30949
oleg.smirnov <oleg.smirnov@exactprosystems.com>
2022-02-17 12:29:35 +0300

    Migrate to newer data-provider gRPC interface

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ setup(
     install_requires=[
         'grpcio-tools==1.33.1',
         'th2-grpc-common~=3.1.2',
-        'th2-grpc-data-provider~=0.1.4'
+        'th2-grpc-data-provider~=1.1.0.dev1857765873'
     ],
     packages=packages,
     package_data=package_data,
