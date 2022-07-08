babeca5b783b244761344de8da66acc1e1ecc218
oleg.smirnov <oleg.smirnov@exactprosystems.com>
2022-07-08 17:28:13 +0400

    Correct grpc common dep for python

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         'th2-grpc-data-provider==1.1.1.dev2636313007',
-        'th2-grpc-common>=3,<4',
+        'th2-grpc-common>=3.11.1,<4',
         'mypy-protobuf==3.2'
     ],
     packages=packages,
